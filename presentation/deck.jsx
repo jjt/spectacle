aead150eb0b157dde9e7a51ef8ebc9a8dd07a3a0
Jason Trill <jason@jasontrill.com>
2015-10-05 00:09:14 -0700

    Add Markdown component
    Defines a <Markdown/> component that uses markdown-to-react-components to target
    spectacle components.

--- presentation/deck.jsx
+++ presentation/deck.jsx
@@ -2,7 +2,7 @@ import React from "react/addons";
 
 import {
   Appear, BlockQuote, Cite, CodePane, Deck, Fill,
-  Heading, Image, Layout, Link, ListItem, List, Quote, Slide, Text
+  Heading, Image, Layout, Link, ListItem, List, Markdown, Quote, Slide, Text
 } from "../src/spectacle";
 
 import preloader from "../src/utils/preloader";
@@ -82,9 +82,29 @@ export default class extends React.Component {
         </Slide>
         <Slide transition={["slide"]} bgColor="black">
           <BlockQuote>
-            <Quote>Wonderfully formatted quotes</Quote>
-            <Cite>Ken Wheeler</Cite>
+            <Quote>Cat ipsum dolor sit amet, hunt anything that moves or hopped up on goofballs hide when guests come over. Hide when guests come over intrigued by the shower, or stare at ceiling climb leg for stretch and use lap as chair.</Quote>
           </BlockQuote>
+        </Slide>
+        <Slide>
+          <Markdown>
+            {`
+#### Markdown text, inline
+##### Sub-heading
+[Bacon ipsum](http://baconipsum.com) dolor amet jerky t-bone fatback, pork alcatra beef ribs bresaola swine pancetta rump shank kielbasa.
+* List
+* ~~Prosciutto~~ shankle _sirloin_
+* Tenderloin **doner** short ribs
+            `}
+          </Markdown>
+        </Slide>
+        <Slide>
+          <Markdown source={require("raw!./markdown-image.md")}/>
+        </Slide>
+        <Slide>
+          <Markdown source={require("raw!./markdown-blockquote.md")}/>
+        </Slide>
+        <Slide>
+          <Markdown source={require("raw!./markdown-table.md")}/>
         </Slide>
         <Slide transition={["slide", "spin"]} bgColor="primary">
           <Heading caps fit size={1} textColor="tertiary">
---
+++ presentation/markdown-blockquote.md
@@ -0,0 +1,3 @@
+#### Markdown blockquote
+
+> Cat ipsum dolor sit amet, hunt anything that moves or hopped up on goofballs
